9ad91cddb86091c2d02006909c6f27701cc6008a
SOYA CLOUD <nyan@soya.moe>
2017-05-11 10:58:09 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>SCCounter Demo</title>
+<link rel="icon" type="image/ico" sizes="16x16" href="bgg.ico">
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<div class="countdown countdown-container container">
+		<div class="row">
+			<div style="padding: 100px; text-align: center">
+				<span style="font-size: 70px; color: #ffffff">Main Title</span><br>
+				<span style="font-size: 30px; color: #ffffff">Sub Title</span>
+			</div>
+		</div>
+		<div class="clock row">
+			<div class="clock-item clock-days countdown-time-value col-sm-6 col-md-3">
+				<div class="wrap">
+					<div class="inner">
+						<div id="canvas-days" class="clock-canvas"></div>
+
+						<div class="text">
+							<p class="val">0</p>
+							<p class="type-days type-time">DAYS</p>
+						</div><!-- /.text -->
+					</div><!-- /.inner -->
+				</div><!-- /.wrap -->
+			</div><!-- /.clock-item -->
+
+			<div class="clock-item clock-hours countdown-time-value col-sm-6 col-md-3">
+				<div class="wrap">
+					<div class="inner">
+						<div id="canvas-hours" class="clock-canvas"></div>
+
+						<div class="text">
+							<p class="val">0</p>
+							<p class="type-hours type-time">HOURS</p>
+						</div><!-- /.text -->
+					</div><!-- /.inner -->
+				</div><!-- /.wrap -->
+			</div><!-- /.clock-item -->
+
+			<div class="clock-item clock-minutes countdown-time-value col-sm-6 col-md-3">
+				<div class="wrap">
+					<div class="inner">
+						<div id="canvas-minutes" class="clock-canvas"></div>
+
+						<div class="text">
+							<p class="val">0</p>
+							<p class="type-minutes type-time">MINUTES</p>
+						</div><!-- /.text -->
+					</div><!-- /.inner -->
+				</div><!-- /.wrap -->
+			</div><!-- /.clock-item -->
+
+			<div class="clock-item clock-seconds countdown-time-value col-sm-6 col-md-3">
+				<div class="wrap">
+					<div class="inner">
+						<div id="canvas-seconds" class="clock-canvas"></div>
+
+						<div class="text">
+							<p class="val">0</p>
+							<p class="type-seconds type-time">SECONDS</p>
+						</div><!-- /.text -->
+					</div><!-- /.inner -->
+				</div><!-- /.wrap -->
+			</div><!-- /.clock-item -->
+		</div><!-- /.clock -->
+		<div class="row">
+			<div style="padding-top: 100px; text-align: center">
+				<span style="font-size: 15px; color: #ffffff">Footer Messages</span>
+			</div>
+		</div>
+	</div><!-- /.countdown-wrapper -->
+	<audio id="player" src="music.mp3" autoplay="autoplay" loop="loop"></audio>
+</body>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script> 
+<script type="text/javascript" src="js/jquery.final-countdown.min.js"></script> 
+<script type="text/javascript" src="js/kinetic.js"></script> 
+<script type="text/javascript" src="js/gulpfile.js"></script> 
+<script type="text/javascript">
+$('document').ready(function() {
+        'use strict';
+        
+    	$('.countdown').final_countdown({
+            'start': 1489330800, // Start time here; Epoch time
+            'end': 1499860800, // End time here; Epoch time
+            'now': new Date().getTime() / 1000 // Current time
+        });
+    });
+</script>
